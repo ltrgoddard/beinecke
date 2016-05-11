cdea9d8209034897b33c30b1d0d1a8f8d2f9f8d9
Louis Goddard <louisgoddard@gmail.com>
2016-05-11 17:01:52 +0000

    line widths based on connection weights

--- beinecke.sh
+++ beinecke.sh
@@ -3,6 +3,6 @@ python3 beinecke.py "H.D. (Hilda Doolittle)" hd.csv hd_data.csv
 python3 beinecke.py "Stein, Gertrude" stein.csv stein_data.csv
 python3 beinecke.py "Williams, William Carlos" williams.csv williams_data.csv
 rm beinecke.csv
-echo "V1,V2,weight" > beinecke.csv
+echo "V1,V2,value" > beinecke.csv
 cat *_data.csv >> beinecke.csv
 
